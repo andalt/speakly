28c0b3915d49e08ef1770a9cc6cec842f9906756
Andrey <andalt11@gmail.com>
2024-08-28 16:35:32 +0300

    implemented common route

--- src/app/_dashboard/DInfo.tsx
+++ src/app/_dashboard/DInfo.tsx
@@ -2,9 +2,11 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import type { Common } from "../api/common/route";
 
 export const DInfo = async () => {
-    const data: Common = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/common`).then((res) => res.json());
+    // const data: Common = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/common`).then((res) => res.json());
 
-    console.log(data, "data");
+    // console.log(data, "data");
+
+    console.log(process.env.NEXT_PUBLIC_APP_URL, "!!!!!");
 
     return (
         <Card>
@@ -12,7 +14,7 @@ export const DInfo = async () => {
                 <CardTitle>Information</CardTitle>
                 {/* <CardDescription>Deploy your new project in one-click.</CardDescription> */}
             </CardHeader>
-            <CardContent>Number of Users: {data.userCount}</CardContent>
+            {/* <CardContent>Number of Users: {data.userCount}</CardContent> */}
             {/* <CardFooter className="flex justify-between">
                 <Button variant="outline">Cancel</Button>
                 <Button>Deploy</Button>
