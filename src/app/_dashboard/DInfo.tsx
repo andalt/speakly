7cbb5892bb885be0ea595ff7a0c9842b9e6b8037
Andrey <andalt11@gmail.com>
2024-08-28 17:44:48 +0300

    added local env

--- src/app/_dashboard/DInfo.tsx
+++ src/app/_dashboard/DInfo.tsx
@@ -2,11 +2,9 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import type { Common } from "../api/common/route";
 
 export const DInfo = async () => {
-    // const data: Common = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/common`).then((res) => res.json());
+    const data: Common = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/common`).then((res) => res.json());
 
-    // console.log(data, "data");
-
-    console.log(process.env.NEXT_PUBLIC_APP_URL, "!!!!!");
+    console.log(data, "data");
 
     return (
         <Card>
@@ -14,7 +12,7 @@ export const DInfo = async () => {
                 <CardTitle>Information</CardTitle>
                 {/* <CardDescription>Deploy your new project in one-click.</CardDescription> */}
             </CardHeader>
-            {/* <CardContent>Number of Users: {data.userCount}</CardContent> */}
+            <CardContent>Number of Users: {data.userCount}</CardContent>
             {/* <CardFooter className="flex justify-between">
                 <Button variant="outline">Cancel</Button>
                 <Button>Deploy</Button>
